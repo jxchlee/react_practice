aafea34656d81dd9e075df75bc1669a86df255d5
CodeSandbox <support@codesandbox.io>
2026-04-11 08:38:11 +0000

    Sandbox snapshot commit

---
+++ first_project/src/components/Search.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  //   searchTerm = "im not BATMAN";
+  return <div className="text-white text-3xl">{searchTerm}</div>;
+};
+
+export default Search;
