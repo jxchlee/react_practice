7fd8470b1b21946027901d8a793ac9da9b7b3de2
CodeSandbox <support@codesandbox.io>
2026-04-11 08:03:56 +0000

    Sandbox snapshot commit

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState, useEffect } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} 컴포넌트 렌더링됨!`);
-    // console.log("씨발");
-  }, [hasLiked]);
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>
-        {title} <br /> {count || null}
-      </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "♡"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   );
 };
 
