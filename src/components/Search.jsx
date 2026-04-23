1900a47990695797a316a8679401d278d8463fc1
CodeSandbox <support@codesandbox.io>
2026-04-23 09:37:58 +0000

    Sandbox snapshot commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  //   searchTerm = "im not BATMAN";
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
